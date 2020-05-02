2b860b738113bbe24b8eea77cd223c9d77b011c3
lidawei0124 <925521373@qq.com>
2020-05-02 19:07:55 +0800

    part61

---
+++ part6/lab6_3/date.h
@@ -0,0 +1,15 @@
+#ifndef DATE_H_
+#define DATE_H_
+
+class Date
+{
+    public:
+        Date(int y=1, int m=1, int d=1) : year(y), month(m), day(d){};
+        int operator-(const Date &a);//重载运算符-，用于计算两个日期的差
+        void show();//显示
+    private:
+        int year;
+        int month;
+        int day;
+};
+#endif
